dec738f85c10dfd84bf09d37f0fd7edcd3e4923f
mqf20 <mingqingfoo@gmail.com>
2018-02-16 12:24:38 +0800

    added more progress for exercise4.java

--- src/week4/exercise4.java
+++ src/week4/exercise4.java
@@ -6,10 +6,13 @@ import java.util.Calendar;
  * Submission by FOO, Ming Qing (1003260, mingqing_foo@mymail.sutd.edu.sg),
  * 
  * The problem with the non-compliant code is: 
- * 
+ * - CalendarSubclass overrides after() so that after() returns true when two objects 
+ *   represent the same date.
+ * - However, after() depends on the compareTo() method, which has also been overridden in
+ *   CalendarSubclass. This causes after() to return the wrong results.
  * 
  * The fix is to: 
- * 
+ * - Implement CalendarSubclass's own comparison.
  */
 public class exercise4 {
   
@@ -19,16 +22,20 @@ public class exercise4 {
 	public static void main (String[] args) {
 	  
         CalendarSubclass cal1 = new CalendarSubclass();
-        System.out.println(cal1);
-        CalendarSubclass cal2 = new CalendarSubclass();
-        System.out.println(cal2.after(cal1));
-        System.out.println(cal2.after(cal2));
-        System.out.println(cal1.after(cal2));
+        cal1.set(1999, 12, 31);
+        System.out.printf("cal1 is %s-%s-%s\n", cal1.get(Calendar.YEAR), cal1.get(Calendar.MONTH), 
+            cal1.get(Calendar.DAY_OF_MONTH));
 
-		System.out.println(cal1.getTime());
-		System.out.println(cal1.hashCode());
-		System.out.println(cal2.getTime());
-		System.out.println(cal2.hashCode());
+        CalendarSubclass cal2 = new CalendarSubclass();
+        cal2.set(2018, 1, 12);
+        System.out.printf("cal2 is %s-%s-%s\n", cal2.get(Calendar.YEAR), cal2.get(Calendar.MONTH), 
+            cal2.get(Calendar.DAY_OF_MONTH));
+        
+        System.out.println("\n>> start of tests...\n");
+        
+//        System.out.println(cal2.after(cal1));  // true
+        System.out.println(cal2.after(cal2));  // true
+//        System.out.println(cal1.after(cal2));  // prints *true* but should be *false*
 
 	}
 }
@@ -36,8 +43,82 @@ public class exercise4 {
 class CalendarSubclass extends Calendar {
 	@Override
 	public boolean after(Object when) {
-		// correctly calls Calendar.compareTo()
-		if (when instanceof Calendar && compareTo((Calendar) when) == 0) {
+
+	    if (!(when instanceof Calendar)) {
+	      throw new IllegalArgumentException();
+	    }
+
+	    Calendar when_calendar = (Calendar) when;
+
+        System.out.printf("this is %s-%s-%s\n", this.get(Calendar.YEAR), this.get(Calendar.MONTH), 
+            this.get(Calendar.DAY_OF_MONTH));
+	    
+        System.out.printf("when_calendar is %s-%s-%s\n", when_calendar.get(Calendar.YEAR), 
+            when_calendar.get(Calendar.MONTH), when_calendar.get(Calendar.DAY_OF_MONTH));
+	    
+	    if (this.get(Calendar.YEAR) > when_calendar.get(Calendar.YEAR)) {
+	      return true;
+	    }
+
+	    if (this.get(Calendar.YEAR) < when_calendar.get(Calendar.YEAR)) {
+	      return false;
+	    }
+	    
+	    if (this.get(Calendar.MONTH) > when_calendar.get(Calendar.MONTH)) {
+	      return true;
+	    }
+
+	    if (this.get(Calendar.MONTH) < when_calendar.get(Calendar.MONTH)) {
+	      return false;
+	    }
+
+	    if (this.get(Calendar.DAY_OF_MONTH) > when_calendar.get(Calendar.DAY_OF_MONTH)) {
+	      return true;
+	    }
+
+	    if (this.get(Calendar.DAY_OF_MONTH) < when_calendar.get(Calendar.DAY_OF_MONTH)) {
+	      return false;
+	    }
+
+	    if (this.get(Calendar.HOUR) > when_calendar.get(Calendar.HOUR)) {
+	      return true;
+	    }
+
+	    if (this.get(Calendar.HOUR) < when_calendar.get(Calendar.HOUR)) {
+	      return false;
+	    }
+
+	    if (this.get(Calendar.MINUTE) > when_calendar.get(Calendar.MINUTE)) {
+	      return true;
+	    }
+
+	    if (this.get(Calendar.MINUTE) < when_calendar.get(Calendar.MINUTE)) {
+	      return false;
+	    }
+
+	    if (this.get(Calendar.SECOND) > when_calendar.get(Calendar.SECOND)) {
+	      return true;
+	    }
+
+	    if (this.get(Calendar.SECOND) < when_calendar.get(Calendar.SECOND)) {
+	      return false;
+	    }
+
+	    if (this.get(Calendar.MILLISECOND) > when_calendar.get(Calendar.MILLISECOND)) {
+	      return true;
+	    }
+
+	    if (this.get(Calendar.MILLISECOND) < when_calendar.get(Calendar.MILLISECOND)) {
+	      return false;
+	    }
+	    
+
+        System.out.println(">> super.compareTo() is " +super.compareTo((Calendar) when));
+        System.out.println(">> super.equals() is " + super.equals((Calendar) when));
+        System.out.println(">> super.before() is " + super.before(when));
+        System.out.println(">> super.after() is " + super.after(when));
+        
+		if (super.compareTo(when_calendar) == 0) {
 			//System.out.println("lala");
 			return true;
 		}
