8ccc8a97c56e963a58be6bd5a44e10c46728054a
mqf20 <mingqingfoo@gmail.com>
2018-03-19 20:19:21 +0800

    added week 9 code

---
+++ src/week9/GA.java
@@ -0,0 +1,23 @@
+package week9;
+
+public class GA {
+
+    public static void main(String[] args) {
+
+        // Set a candidate solution
+        FitnessCalc.setSolution("This is the string test input that we wanted!");
+
+        // Create an initial population
+        Population myPop = new Population(50, true);
+        
+        // Evolve our population until we reach an optimum solution
+        int generationCount = 0;
+        while (myPop.getFittest().getFitness() < FitnessCalc.getMaxFitness()) {
+            generationCount++;
+            System.out.println("Generation: " + generationCount + " Fittest: " + myPop.getFittest().getFitness());
+            System.out.println("Current Fittest: " + myPop.getFittest());
+            myPop = Algorithm.evolvePopulation(myPop);
+        }
+        System.out.println("\nSolution: " + myPop.getFittest());
+    }
+}
