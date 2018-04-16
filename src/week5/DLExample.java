dcbc7fce7ea3b06c4fbc4aef65ece5b155dd62a7
mqf20 <mingqingfoo@gmail.com>
2018-04-16 20:09:15 +0800

    added ex1 for week 13

--- src/week5/DLExample.java
+++ src/week5/DLExample.java
@@ -2,9 +2,65 @@ package week5;
 
 import java.util.HashSet;
 import java.util.Set;
+import org.junit.Test;
 
 public class DLExample {
-	
+    
+    @Test
+    public void testDeadlokcing() {
+        
+        System.out.println(">> starting to test");
+        
+        int NUM_THREADS = 100;
+        
+        Dispatcher dispatcher = new Dispatcher();
+        Taxi taxi = new Taxi(dispatcher);
+        dispatcher.notifyAvailable(taxi);
+
+        Runnable deadlockingTaxi = new Runnable() {
+            @Override
+            public void run() {
+                taxi.setLocation(new Point());
+            }
+        };
+
+        Runnable deadlockingDispatcher = new Runnable() {
+            @Override
+            public void run() {
+                dispatcher.getImage();
+            }
+        };
+        
+        Thread[] threads = new Thread[NUM_THREADS];
+        
+        for (int i = 0; i < NUM_THREADS/2; i++) {
+            
+            System.out.println(">> running threads " + (i*2) + " and " + (i*2+1));
+            
+            threads[i*2] = new Thread(deadlockingTaxi);
+            threads[i*2+1] = new Thread(deadlockingDispatcher);
+            threads[i*2].start();
+            threads[i*2+1].start();
+            
+        }
+
+        for (int i = 0; i < NUM_THREADS/2; i++) {
+            
+            try {
+                threads[i*2].join();
+                System.out.println(">> joined thread " + (i*2));
+                threads[i*2+1].join();
+                System.out.println(">> joined thread " + (i*2+1));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            
+        }
+        
+        System.out.println(">> the end!");
+
+    }
+
 }
 
 class Taxi {
